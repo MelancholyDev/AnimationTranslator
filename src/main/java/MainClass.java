f835c5d6db7e9a83bbe9cda11c3d8de7b0df6f3a
MelancholyDev <morriganisdream@gmail.com>
2022-01-24 14:31:33 +0300

    fix

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public class MainClass {
+    public static void main(String[] args) throws IOException {
+        Process process = Runtime.getRuntime().exec("D:\\Programming\\AnimationTranslator\\src\\main\\resources\\TranslatorAnaconda D:\\Programming\\AnimationTranslator\\src\\main\\resources\\main.py");
+    }
+}
