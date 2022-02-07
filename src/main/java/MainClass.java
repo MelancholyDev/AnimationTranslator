4c03e45dcfd8a4bf3c63d00c9c9294f8b3b15eca
Rabizov_Anton <anrabizov@edu.hse.ru>
2022-02-08 02:08:11 +0300

    added simple ui form

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,7 +1,20 @@
 import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class MainClass {
+public class MainClass extends Application{
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainClass.class.getResource("UI.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
     public static void main(String[] args) throws IOException {
+        launch();
         Process process = Runtime.getRuntime().exec("D:\\Programming\\AnimationTranslator\\src\\main\\resources\\TranslatorAnaconda D:\\Programming\\AnimationTranslator\\src\\main\\resources\\main.py");
     }
 }
