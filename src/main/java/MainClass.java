dfdd7b591b0ce0e4c7b8e7e0f39fb64286e70d58
Rabizov_Anton <anrabizov@edu.hse.ru>
2022-02-18 03:42:12 +0300

    ui upd

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -4,12 +4,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class MainClass extends Application{
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainClass.class.getResource("UI.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 700, 400);
+        stage.setTitle("AnimationTranslator!");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/UIController.java
+++ src/main/java/UIController.java
@@ -1,5 +1,25 @@
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class UIController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button StartPage_Next_Button;
+
+    @FXML
+    void initialize() {
+        StartPage_Next_Button.setOnAction(event -> {
+            System.out.println("Next");
+        });
+
+    }
+
 }
