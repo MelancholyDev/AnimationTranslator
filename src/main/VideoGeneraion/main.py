a9899a1b6854e6c6ca34edd109026258dc1ca971
MelancholyDev <morriganisdream@gmail.com>
2022-03-08 18:03:55 +0300

    add library

--- src/main/VideoGeneraion/main.py
+++ src/main/VideoGeneraion/main.py
@@ -9,19 +9,23 @@ model_path = sys.argv[1]
 image_path = sys.argv[2]
 audio_path = sys.argv[3]
 save_video_path = sys.argv[4]
-is_add_subtitles = sys.argv[5] == 1
+is_add_subtitles = sys.argv[5] == '1'
 va = sda.VideoAnimator(model_path=model_path)
 vid, aud = va(image_path, audio_path)
 va.save_video(vid, aud, save_video_path)
 
-if(is_add_subtitles):
+if (is_add_subtitles):
     generator = lambda txt: moviepy.video.VideoClip.TextClip(txt, font="Times-Roman", fontsize=24, color="White")
     subs = [((0, 4), 'subs1'),
-    ((4, 9), 'subs2'),
-    ((9, 12), 'subs3'),
-    ((12, 16), 'subs4')]
+            ((4, 9), 'subs2'),
+            ((9, 12), 'subs3'),
+            ((12, 16), 'subs4')]
     subtitles = moviepy.video.tools.subtitles.SubtitlesClip(subs, generator)
     video = moviepy.video.io.VideoFileClip.VideoFileClip(save_video_path)
-    result = moviepy.video.compositing.CompositeVideoClip.CompositeVideoClip([video, subtitles.set_pos(('center', 'bottom'))])
-    result.write_videofile(save_video_path+"_subs", fps=video.fps, temp_audiofile="temp-audio.m4a", remove_temp=True, codec="libx264",
-    audio_codec="aac")
+    final_path = save_video_path[:-4]
+    final_path = final_path + '_subs.mp4'
+    result = moviepy.video.compositing.CompositeVideoClip.CompositeVideoClip(
+        [video, subtitles.set_pos(('center', 'bottom'))])
+    result.write_videofile(final_path, fps=video.fps, temp_audiofile="temp-audio.m4a", remove_temp=True,
+                           codec="libx264",
+                           audio_codec="aac")
